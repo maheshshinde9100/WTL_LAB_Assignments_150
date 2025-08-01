4456d3c68a285499cafa4fb061e1e81d47486127
MAHESH <maheshshinde9100@gmail.com>
2025-08-01 18:40:27 +0530

    intial commit for server.js

---
+++ backend-practice/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors');
+
+const PORT = 5000;
+
+const MongoURI = "mongodb://localhost:27017/practice-nodejs";
+
+const app = express()
+
+app.use(express.json())
+// app.use(cors)
+
+mongoose.connect(MongoURI)
+    .then(() => console.log("Mongodb connected successfully..: "))
+    .catch(err => console.log("Mongodb connection error..: ",err))
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
